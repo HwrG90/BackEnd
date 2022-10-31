dd3a96aeed75d75d0f23abc596d3dc39a5799a91
HwrG90 <95428144+HwrG90@users.noreply.github.com>
2022-10-31 01:35:12 -0300

    Desafio-Clase-Veintiocho

--- model/usuario.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const UsuarioSchema = new mongoose.Schema({
-  nombre: { type: String, required: true },
-  correo: { type: String, required: true, unique: true },
-  contraseña: { type: String, required: true },
-});
-
-module.exports = mongoose.model("Usuario", UsuarioSchema);
